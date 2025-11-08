cc376ee67e872de04f322acf4563c8166c525d36
Dhanush <kumardhanush730@gmail.com>
2025-11-08 19:52:39 +0530

    Update experiences to reflect new job details

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,15 +1,8 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Web Developer Intern',
-    company: "THE FOLKS COMPANY",
-    duration: "(Dec 2024 - Apr 2025)"
-  },
-  {
-    id: 2,
-    title: 'Full Stack Developer',
-    company: "SKSY SELLERFLY ONLINE SOLUTIONS LLP ©",
-    duration: "(July 2025 - Sept 2025)"
-  },
- 
+    title: 'Software Developer SDE1',
+    company: "Intellicar",
+    duration: "(Oct 2025 - Present)"
+  }
 ]
