7e14f92d9a1abcedba43e2d21b8e7f8979954de5
Dhanush <kumardhanush730@gmail.com>
2025-11-08 19:55:00 +0530

    bengalore

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,7 +2,8 @@ export const experiences = [
   {
     id: 1,
     title: 'Software Developer SDE1',
-    company: "Intellicar",
+    company: "Intellicar, Bengalore",
     duration: "(Oct 2025 - Present)"
   }
 ]
+
